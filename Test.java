e0924493a622b288eb035974b46614129f404c3e
bdisptn <budisptn13@gmail.com>
2025-09-25 10:12:32 +0700

    kenalan

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama saya");
         System.out.println("Hi saya Fafa, ini baris kode pertama saya");
+
+        System.out.println("Halo fafa, saya budi");
     }
 }
